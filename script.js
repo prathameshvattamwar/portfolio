66e4c58ec6903ca9c5941387655c1e13a4096c50
Prathamesh Vattamwar <117809192+prathameshvattamwar@users.noreply.github.com>
2023-11-01 21:49:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+
+}
+
+var sidemenu = document.getElementById("sidemenu");
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+function openmenu(){
+    sidemenu.style.right = 0;
+}
